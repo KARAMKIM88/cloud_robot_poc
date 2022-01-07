63794d67cd175add4a9ea565da2886073e2f5202
user <karamkim88@lguplus.co.kr>
2022-01-08 01:23:28 +0900

    first commit

---
+++ run_multiprocess.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import subprocess
+import http.server
+import socketserver
+import os
+import time
+
+
+os.system('roscore &' )
+time.sleep(5)
+os.system('rosrun web_video_server web_video_server &' )
+os.system('rosrun video_stream_opencv video_stream &' )
+
+"""
+subprocess.Popen(['roscore'])
+subprocess.Popen(['rosrun web_video_server web_video_server'])
+subprocess.Popen(['rosrun video_stream_opencv video_stream_opencv'])
+"""
+
+
+
+handler = http.server.SimpleHTTPRequestHandler
+
+HOST, PORT = "localhost", 10808
+
+with socketserver.TCPServer((HOST, PORT), handler) as httpd:
+  print('Server listening on port 10808...')
+  httpd.serve_forever()
